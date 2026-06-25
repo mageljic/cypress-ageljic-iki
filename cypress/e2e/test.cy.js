c80b9569019ca0c967622f3fdab1e413dc84ce9d
Monika Ageljić <mageljic@ffos.hr>
2026-06-25 13:17:07 +0200

    iki cypress

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,40 @@
+describe('Preuzimanje rasporeda sati za za studij informatologije', () => {
+
+  it('student pronalazi odsjek i preuzima raspored sati', () => {
+
+      cy.viewport(1920, 1080)
+      
+    cy.visit('https://www.ffos.unios.hr/')
+
+    cy.contains('Odsjeci i katedre')
+    .realHover()
+
+    cy.contains('Odsjek za informacijske znanosti')
+    .should('be.visible')
+    .click()
+
+  
+
+    cy.contains('Raspored sati')
+      .scrollIntoView()
+
+    cy.contains('Raspored sati')
+    .invoke('removeAttr', 'target')
+    .click()
+
+    cy.contains('Raspored sati')
+      .click()
+
+     
+
+    cy.contains('Informatologija, Nakladništvo i Informacijske tehnologije')
+    .invoke('attr', 'href')
+    .then((href) => {
+    cy.request(href)
+      .its('status')
+      .should('eq', 200)
+  })
+
+  })
+
+})
